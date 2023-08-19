c1cc79dac560ac4d57f7ab33a3b2b71a8650e299
maultart <maultart@student.21-school.ru>
2023-08-19 19:51:36 +0700

    Change static memory to a dynamic one

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -8,12 +8,13 @@
 #define M 80
 #define DEFAULT_CONFIG "config1.txt"
 
-int positive_mod(int divisor, int denominator);
-int check_adjasents(char *matrix, int n, int m, int i, int j);
+int mod(int divisor, int denominator);
+int check_adjasents(char **matrix, int n, int m, int i, int j);
 
-int draw_and_update(char *field, int n, int m, int count);
+int draw_and_update(char **field, int n, int m, int count);
+int input(char **field, int n, int m, int *count_ptr);
 
-int input(char *field, int n, int m, int *count_ptr);
+char **allocate_matrix(int n, int m);
 
 int main(int argc, char **argv) {
     int exit_status = EXIT_SUCCESS;
@@ -26,35 +27,49 @@ int main(int argc, char **argv) {
         printf("Configuration file '%s' is passed.\n", argv[1]);
         config = freopen(argv[1], "r", stdin);
     }
-    if (config != NULL) {
-        char field[N][M];  // 0 - dead, 1 - alive
-        int n = N, m = M;
 
+    int n = N, m = M;
+    char **field = allocate_matrix(n, m);  // 0 - dead, 1 - alive
+    if (config != NULL && field != NULL) {
         int count = 0;
-        if (input((char *)field, n, m, &count) == EXIT_SUCCESS) {
-            while (count >= 0) {
-                count = draw_and_update((char *)field, n, m, count);
+        if (input(field, n, m, &count) == EXIT_SUCCESS) {
+            while (count >= 0 && exit_status == EXIT_SUCCESS) {
+                count = draw_and_update(field, n, m, count);
+                if (count == -1) exit_status = EXIT_FAILURE;
             }
         }
+        free(field);
         fclose(config);
     } else {
-        perror("An error has occured");
+        if (field) free(field);
+        if (config) fclose(config);
         exit_status = EXIT_FAILURE;
     }
+
+    if (exit_status != EXIT_SUCCESS) perror("An error has occured");
 
     return exit_status;
 }
 
-char *get(char *field, int i, int j) { return field + i * M + j; }
+// Matrix allocation
 
-int positive_mod(int divisor, int denominator) { return (divisor % denominator + denominator) % denominator; }
+char **allocate_matrix(int n, int m) {
+    char **matrix = malloc(n * m * sizeof(char) + n * sizeof(char *));
+    for (int i = 0; i < n; i++) matrix[i] = ((char *)(matrix + n)) + m * i;
+    memset(matrix + n, 0, n * m * sizeof(char));
+    return matrix;
+}
 
-int check_adjasents(char *matrix, int n, int m, int i, int j) {
+// Secondary functions
+
+int mod(int divisor, int denominator) { return (divisor % denominator + denominator) % denominator; }
+
+int check_adjasents(char **matrix, int n, int m, int i, int j) {
     int counter = 0;
     for (int di = -1; di <= 1; di++) {
         for (int dj = -1; dj <= 1; dj++) {
             if (di == 0 && dj == 0) continue;
-            if (*get(matrix, positive_mod(i + di, n), positive_mod(j + dj, m))) counter++;
+            if (matrix[mod(i + di, n)][mod(j + dj, m)]) counter++;
         }
     }
     return counter;
@@ -68,43 +83,46 @@ void print_horizontal_border(int w) {
     printf("+\n");
 }
 
-int input(char *field, int n, int m, int *count) {
+int input(char **field, int n, int m, int *count) {
     int exit_status = EXIT_SUCCESS;
     for (int i = 0; i < n && !exit_status; i++) {
         for (int j = 0; j < m && !exit_status; j++) {
-            if (scanf(" %c", get(field, i, j)) != 1 ||
-                !(*get(field, i, j) == '0' || *get(field, i, j) == '1'))
+            if (scanf(" %c", field[i] + j) != 1 || !(field[i][j] == '0' || field[i][j] == '1'))
                 exit_status = EXIT_FAILURE;
-            *get(field, i, j) -= '0';
-            if (*get(field, i, j)) (*count)++;
+            field[i][j] -= '0';
+            if (field[i][j]) (*count)++;
         }
     }
     return exit_status;
 }
 
-int draw_and_update(char *field, int n, int m, int count) {
-    char next_field[N][M] = {0};
-    printf("\033c");
-    print_horizontal_border(m);
-    for (int i = 0; i < n; i++) {
-        printf("|");
-        for (int j = 0; j < m; j++) {
-            printf("%c", *get(field, i, j) ? 'o' : ' ');
-            int neighbours = check_adjasents((char *)field, n, m, i, j);
-            if (neighbours == 3 || (*get(field, i, j) && neighbours == 2))
-                next_field[i][j] = 1;
-            else
-                next_field[i][j] = 0;
+int draw_and_update(char **field, int n, int m, int count) {
+    char **next_field = allocate_matrix(n, m);
+    if (next_field != NULL) {
+        printf("\033c");
+        print_horizontal_border(m);
+        for (int i = 0; i < n; i++) {
+            printf("|");
+            for (int j = 0; j < m; j++) {
+                printf("%c", field[i][j] ? 'o' : ' ');
+                int neighbours = check_adjasents(field, n, m, i, j);
+                if (neighbours == 3 || (field[i][j] && neighbours == 2))
+                    next_field[i][j] = 1;
+                else
+                    next_field[i][j] = 0;
 
-            if (*get(field, i, j) && !next_field[i][j])
-                count--;
-            else if (!*get(field, i, j) && next_field[i][j])
-                count++;
+                if (field[i][j] && !next_field[i][j])
+                    count--;
+                else if (!field[i][j] && next_field[i][j])
+                    count++;
+            }
+            puts("|");
         }
-        puts("|");
-    }
-    print_horizontal_border(m);
-    memcpy(field, (char *)next_field, n * m * sizeof(char));
-    usleep(500000);
+        print_horizontal_border(m);
+        memcpy(field + n, next_field + n, n * m * sizeof(char));
+        usleep(500000);
+        free(next_field);
+    } else
+        count = -1;
     return count;
 }
