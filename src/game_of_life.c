244a66d9b4aeaddda4f1c59982d911c3aab5b9b4
maultart <maultart@student.21-school.ru>
2023-08-19 18:22:20 +0700

    Separate everything on different functions

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,68 +1,101 @@
+#include <ncurses.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #define N 25
 #define M 80
 
-int pos_mod(int divisor, int denominator) { return (divisor % denominator + denominator) % denominator; }
+int positive_mod(int divisor, int denominator);
+int check_adjasents(char *matrix, int n, int m, int i, int j);
+
+int draw_and_update(char *field, int n, int m, int count);
+
+int input(char *field, int n, int m, int *count_ptr);
+
+int main(void) {
+    int exit_status = EXIT_SUCCESS;
+
+    FILE *config;
+    if ((config = freopen("config1.txt", "r", stdin)) != NULL) {
+        char field[N][M];  // 0 - dead, 1 - alive
+        int n = N, m = M;
+
+        int count = 0;
+        if (input((char *)field, n, m, &count) == EXIT_SUCCESS) {
+            while (count >= 0) {
+                count = draw_and_update((char *)field, n, m, count);
+            }
+        }
+        fclose(config);
+    } else {
+        perror("An error has occured: ");
+        exit_status = EXIT_FAILURE;
+    }
+
+    return exit_status;
+}
+
+char *get(char *field, int i, int j) { return field + i * M + j; }
+
+int positive_mod(int divisor, int denominator) { return (divisor % denominator + denominator) % denominator; }
 
 int check_adjasents(char *matrix, int n, int m, int i, int j) {
     int counter = 0;
     for (int di = -1; di <= 1; di++) {
         for (int dj = -1; dj <= 1; dj++) {
             if (di == 0 && dj == 0) continue;
-            if (matrix[pos_mod(i + di, n) * m + pos_mod(j + dj, m)]) counter++;
+            if (*get(matrix, positive_mod(i + di, n), positive_mod(j + dj, m))) counter++;
         }
     }
     return counter;
 }
 
-int main(void) {
-    char field[N][M];  // 0 - dead, 1 - alive
-    char next_field[N][M];
-    int n = N, m = M;
+// Input output section
 
-    if (freopen("config1.txt", "r", stdin) == NULL) return -1;
+void print_horizontal_border(int w) {
+    printf("+");
+    for (int i = 0; i < w; i++) printf("-");
+    printf("+\n");
+}
 
-    int count = 0;
+int input(char *field, int n, int m, int *count) {
+    int exit_status = EXIT_SUCCESS;
+    for (int i = 0; i < n && !exit_status; i++) {
+        for (int j = 0; j < m && !exit_status; j++) {
+            if (scanf(" %c", get(field, i, j)) != 1 ||
+                !(*get(field, i, j) == '0' || *get(field, i, j) == '1'))
+                exit_status = EXIT_FAILURE;
+            *get(field, i, j) -= '0';
+            if (*get(field, i, j)) (*count)++;
+        }
+    }
+    return exit_status;
+}
+
+int draw_and_update(char *field, int n, int m, int count) {
+    char next_field[N][M] = {0};
+    print_horizontal_border(m);
     for (int i = 0; i < n; i++) {
+        printf("|");
         for (int j = 0; j < m; j++) {
-            scanf(" %c", field[i] + j);
-            field[i][j] -= '0';
-            if (field[i][j]) count++;
+            printf("%c", *get(field, i, j) ? 'o' : ' ');
+            int neighbours = check_adjasents((char *)field, n, m, i, j);
+            if (neighbours == 3 || (*get(field, i, j) && neighbours == 2))
+                next_field[i][j] = 1;
+            else
+                next_field[i][j] = 0;
+
+            if (*get(field, i, j) && !next_field[i][j])
+                count--;
+            else if (!*get(field, i, j) && next_field[i][j])
+                count++;
         }
+        puts("|");
     }
-
-    while (count) {
-        printf("+");
-        for (int i = 0; i < m; i++) printf("-");
-        printf("+\n");
-        for (int i = 0; i < n; i++) {
-            printf("|");
-            for (int j = 0; j < m; j++) {
-                printf("%c", field[i][j] ? 'o' : ' ');
-                int neighbours = check_adjasents((char *)field, n, m, i, j);
-                if (field[i][j] && neighbours >= 2 && neighbours <= 3)
-                    next_field[i][j] = 1;
-                else if (!field[i][j] && neighbours == 3)
-                    next_field[i][j] = 1;
-                else
-                    next_field[i][j] = 0;
-
-                if (field[i][j] && !next_field[i][j])
-                    count--;
-                else if (!field[i][j] && next_field[i][j])
-                    count++;
-            }
-            puts("|");
-        }
-        printf("+");
-        for (int i = 0; i < m; i++) printf("-");
-        printf("+\n");
-        memcpy(field, next_field, n * m * sizeof(field[0][0]));
-        sleep(1);
-    }
-
-    return 0;
+    print_horizontal_border(m);
+    memcpy(field, (char *)next_field, n * m * sizeof(char));
+    usleep(500000);
+    return count;
 }
